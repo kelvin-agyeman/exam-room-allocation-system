0d183a98f954a2ca8c5382de97a1189fd44e034b
Kelvin1237 <kelvinagyeman11@gmail.com>
2026-03-29 13:33:00 +0000

    Fixed sending emails

--- utils/sendResetPasswordEmail.js
+++ utils/sendResetPasswordEmail.js
@@ -1,5 +1,18 @@
-import { sendEmail } from "./sendEmail.js";
+import nodemailer from "nodemailer";
 import { resetPasswordEmailHTML } from "./constants.js";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.APP_PASSWORD,
+  },
+});
 
 export const sendResetPasswordEmail = async ({
   name,
@@ -9,9 +22,13 @@ export const sendResetPasswordEmail = async ({
 }) => {
   const resetURL = `${origin}/student/reset-password?token=${token}&email=${email}`;
 
-  return sendEmail({
+  await transporter.sendMail({
+    from: {
+      name: "Exam Room Allocation Portal",
+      address: process.env.EMAIL_USER,
+    },
     to: email,
-    subject: "Reset Password",
+    subject: "Password Reset",
     html: resetPasswordEmailHTML({ name, resetURL }),
   });
 };
