f8952d14791610737bc6c1463bd956821ebf3db0
Kelvin1237 <kelvinagyeman11@gmail.com>
2026-03-29 13:57:43 +0000

    fixed nodemailer transporter

--- utils/sendResetPasswordEmail.js
+++ utils/sendResetPasswordEmail.js
@@ -6,8 +6,8 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   service: "gmail",
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.APP_PASSWORD,
